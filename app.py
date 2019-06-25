cacce47ff1597a9a500a4d251d3efb37e9c3629a
Maya <mayaspencerjacobs1@gmail.com>
2019-06-25 18:21:00 +0100

    WORKING: posts all records to page

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from forms import CreateForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'amayamaya'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-posts = [
-    {
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018',
+class Items(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), unique=False, nullable=False)
+    content = db.Column(db.String(120), unique=True, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    complete = db.Column(db.Boolean, unique=False, default=False)
 
-    },
-    {
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-
-    }
-]
-
+    def __init__(self,title,content):
+        self.title = title
+        self.content = content
 
 @app.route("/")
 @app.route("/view_all")
 def view_all():
+
+    posts = Items.query.all()
     return render_template('view_all.html', posts=posts)
 
 
 @app.route("/view_completed")
 def view_completed():
-    return render_template('view_completed.html', title='About')
+    posts = Items.query.all()
+    return render_template('view_completed.html', posts=posts)
 
 
 
@@ -35,13 +38,13 @@ def view_completed():
 def create():
     form = CreateForm()
     if form.validate_on_submit():
-        return redirect(url_for('home'))
-    else:
-        flash('Post Unsuccessful. Please check minimum requirements', 'danger')
-    return render_template('create.html', title='Create', form=form)
-
-
-
+        post = Items(title=form.title.data, content=form.content.data)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('view_all'))
+    return render_template('create.html', title='New Post',
+                           form=form, legend='New Post')
 
 
 if __name__ == '__main__':
