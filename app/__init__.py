86a152138ebb10ecd89be3df155f3849e4322bbb
Maya <mayaspencerjacobs1@gmail.com>
2019-10-26 14:32:13 +0100

    flask form
    version had decpreciated

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
+#from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+#app = Flask(__name__)
+#app.config.from_object('config_app')
+#db = SQLAlchemy(app)
+#from app import views, models
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
 
 app = Flask(__name__)
 app.config.from_object('config_app')
-
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 from app import views, models
--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 #FORM TO CREATE TO DO ITEMS
 #THIS IS CALLED ON IN APP.PY
-class CreateForm(Form):
+class CreateForm(FlaskForm):
     title = StringField('Title',
                            validators=[DataRequired(), Length(min=2, max=20)])
     content = StringField('Content', validators=[DataRequired(), Length(min=2, max=80)])
