f84ca0f98c86159a9842fe3e6ccb2d84411428a8
Maya <mayaspencerjacobs1@gmail.com>
2019-10-26 15:51:58 +0100

    migrating updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,3 @@
-#from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
-#app = Flask(__name__)
-#app.config.from_object('config_app')
-#db = SQLAlchemy(app)
-#from app import views, models
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
---
+++ db_create.py
@@ -0,0 +1,8 @@
+from config_app import SQLALCHEMY_DATABASE_URI
+
+from app import db
+import os.path
+
+
+# Creates all the tables and the database.
+db.create_all()
